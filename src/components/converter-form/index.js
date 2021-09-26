122d879bdf0a7534556c30db9e8553d1f5bbe085
Ariel Batislaon <ariel.batislaon@gbst.com>
2021-09-27 01:29:47 +1000

    second commit

--- src/components/converter-form/index.js
+++ src/components/converter-form/index.js
@@ -1,30 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types';
 import CurrencyAmount from '../currency-amount'
 import CurrencySelector from '../currency-selector'
 import ConvertedAmount from '../converted-amount'
+import { LoadingSpinner } from '../loading-spinner';
 import { getCurrencyOptions, getCalculatedRateFromAPI, validateAmount } from '../../utils'
 import {CURRENCY_LIST, CURRENCY_CALCULATOR_API_URL} from '../../config'
 
 function ConverterForm(props) {
-    const {labels} = props;
+   const {labels} = props;
    const [error, setError] = useState('');
    const [amountValue, setAmountValue] = useState(1.0);
    const [from, setFrom] = useState(null);
    const [to, setTo] = useState(null);
    const [resultValue, setResultValue] = useState(null);
+   const [loading, setLoading] = useState(false);
+   const [calculate, setCalculate] = useState(false);
    
    
-   const submitForm = () => {
-    const url = `${CURRENCY_CALCULATOR_API_URL}/calculate-currency/?fromCurrency=${from.label}&toCurrency=${to.label}&amount=${amountValue}`;
-    getCalculatedRateFromAPI(url).then(response => {
-        console.log(response);
-        setResultValue(response.data.calculatedAmount);
-    })
-    .catch(err => {
-        console.log(err);
-      });
-           
+   const submitForm = (e) => {
+    e.preventDefault();
+    setCalculate(true)
    }
 
    const onAmountChange = (e) => {
@@ -37,6 +33,21 @@ function ConverterForm(props) {
     setFrom(fromValue);
     setTo(null);;
    } 
+   useEffect(() => {
+       if(calculate) {
+        const url = `${CURRENCY_CALCULATOR_API_URL}/calculate-currency/?fromCurrency=${from.label}&toCurrency=${to.label}&amount=${amountValue}`;
+        getCalculatedRateFromAPI(url).then(response => {
+            console.log(response);
+            setResultValue(response.data.calculatedAmount);
+            setCalculate(false);
+        })
+        .catch(err => {
+            console.log(err);
+            setLoading(false);
+          });
+       }
+  
+   }, [calculate])
  
     return (
   
@@ -58,9 +69,8 @@ function ConverterForm(props) {
             value={to}
             onChange={toValue => setTo(toValue)}
             />
-            <button type="submit">
-            {labels.calculateButtonText}
-          </button>
+            <button type="submit">{labels.calculateButtonText}</button>
+            {calculate && <LoadingSpinner /> } 
             <ConvertedAmount value={resultValue} />
         </form>
        
---
+++ src/components/loading-spinner/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+export const LoadingSpinner = () =>
+  <i className="loading-icon fa fa-spinner fa-pulse fa-3x fa-fw" />;
